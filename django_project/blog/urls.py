fee72d3072d197ae113589a15d4eb1ce3863f5ed
john8739 <jsolly@esri.com>
2020-08-08 19:07:45 -0700

    First commit!

---
+++ django_project/blog/urls.py
@@ -0,0 +1,38 @@
+"""django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    CreatePostView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+    CreateCommentView,
+)
+from . import views
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="blog-home"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("about/", views.about, name="blog-about"),
+    path("post/new", CreatePostView.as_view(), name="post-create"),
+    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
+    path("post/<int:pk>/comment/", CreateCommentView.as_view(), name="comment-create"),
+]
---
+++ django_project/blog/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Post, Comment
+from django.forms import widgets
+
+
+def home(request):
+    context = {"posts": Post.objects.all()}
+    return render(request, "blog/home.html", context=context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"  # The default is object_list
+    ordering = ["-date_posted"]
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"  # The default is object_list
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class CreateCommentView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ["content"]
+
+    def form_valid(self, form):
+        form.instance.post = Post.objects.get(id=self.kwargs["pk"])
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView, LoginRequiredMixin, UserPassesTestMixin):
+
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About"})
