090435a87c8d2437d2e007562cf0f90793456ba6
John Solly <jsolly@pm.me>
2022-02-13 18:36:01 -0800

    chore: Modify STATIC_ROOT path for production

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG: 
+    STATIC_ROOT = "/home/jsolly/blogthedata/django_project/staticfiles"
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
