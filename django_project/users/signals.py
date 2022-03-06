8dee25f5c408cfcce85c6d75ba2ba0539524b100
John Solly <jsolly@protonmail.com>
2022-03-06 10:56:53 -0800

    revert: :rewind: uncomment code that was commented out during database migration

--- django_project/users/signals.py
+++ django_project/users/signals.py
@@ -4,12 +4,12 @@ from django.dispatch import receiver
 from .models import Profile
 
 
-# @receiver(post_save, sender=User)
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 
-# @receiver(post_save, sender=User)
-# def save_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
