08739dccbf7f3a3830d8b7b905dc82ff51835719
John Solly <jsolly@protonmail.com>
2022-04-13 07:12:15 -0700

    style: :lipstick: change post category productivity to life advice

--- django_project/blog/forms.py
+++ django_project/blog/forms.py
@@ -2,16 +2,18 @@ from django import forms
 from .models import Post, Comment, Category
 
 choices = [
-    ("productivity", "productivity"),
-    ("aphorisms", "aphorisms"),
+    ("life advice", "life advice"),
     ("site updates", "site updates"),
 ]
-#choices = Category.objects.all().values_list('name', 'name') # comment this if doing an initial DB migration or changing databases
-# I don't think I am doing 
+# choices = Category.objects.all().values_list('name', 'name') # comment this if doing an initial DB migration or changing databases
+# I don't think I am doing
+
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("title", "slug", "category", "metadesc", "draft", "metaimg", "content", "snippet",)
+        fields = ("title", "slug", "category", "metadesc",
+                  "draft", "metaimg", "content", "snippet",)
 
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control"}),
