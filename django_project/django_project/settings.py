bd104fd94b8012f5afca2fb4d8d235a449b350ba
John Solly <jsolly@protonmail.com>
2022-05-06 06:10:27 -0700

    feat: :heavy_plus_sign: Change email service to sendgrid

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -196,11 +196,12 @@ LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config.get("FROM_EMAIL")
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 CKEDITOR_CONFIGS = {
