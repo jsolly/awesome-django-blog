d6f6f0af446304dd331cf1c2d6b0aae80da8ec86
John Solly <jsolly@protonmail.com>
2022-05-16 22:45:51 -0700

    style: :rotating_light:

--- django_project/django_project/asgi.py
+++ django_project/django_project/asgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-#import os
+# import os
 
 from django.core.asgi import get_asgi_application
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
 application = get_asgi_application()
