916751a9c79f9efa565f1eab74aca60adcf2bdb3
John Solly <jsolly@protonmail.com>
2022-05-19 09:12:53 -0700

    feat: :lipstick: Add first_name and last_name to user model and reference this in templates

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -7,12 +7,15 @@ from captcha.fields import CaptchaField
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+
     secret_password = forms.CharField()
     captcha = CaptchaField()
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
 
 
 class UserUpdateForm(forms.ModelForm):
