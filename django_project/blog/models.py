ed5e19127b7a4ddc000bb7320a05c10ab6f8c059
John Solly <jsolly@protonmail.com>
2022-06-08 07:44:19 -0700

    refactor: :wheelchair: Change metaimg-alt text length from 60 to 120

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -38,7 +38,7 @@ class Post(models.Model):
     draft = models.BooleanField(default=False)
     metaimg = models.ImageField(default="jsolly.jpeg", upload_to="post_metaimgs/")
     metaimg_mimetype = models.CharField(max_length=20, default="image/jpeg")
-    metaimg_alt_txt = models.CharField(max_length=60, default="Meta Image")
+    metaimg_alt_txt = models.CharField(max_length=120, default="Meta Image")
     content = CKEditor5Field(blank=True, null=True, config_name='extends')
     snippet = CKEditor5Field(max_length=300, blank=True, null=True, config_name='extends')
     date_posted = models.DateTimeField(default=timezone.now)
