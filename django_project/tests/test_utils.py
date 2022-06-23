7de257c6531b3eea7e8b4415495056e7688d3a38
John Solly <jsolly@protonmail.com>
2022-06-23 08:15:21 -0700

    add tests

--- django_project/tests/test_utils.py
+++ django_project/tests/test_utils.py
@@ -1,7 +1,10 @@
 from .base import SetUp
 from django.utils.deprecation import MiddlewareMixin
 from blog.utils import slugify_instance_title
-
+from siteanalytics.utils import get_IP_details, load_data
+from siteanalytics.models import Visitor
+from django.contrib.gis.geos import Point
+import os
 
 class TestUtils(SetUp, MiddlewareMixin):
     """Tests for helper functions"""
@@ -9,3 +12,14 @@ class TestUtils(SetUp, MiddlewareMixin):
     def test_slugify_instance_title(self):
         slugify_instance_title(self.post1, new_slug="My-First-Post", save=True)
         self.assertEqual(self.post1.slug, "My-First-Post")
+
+    def test_get_IP_details(self):
+        access_token = os.environ.get["IP_INFO_TOKEN"]
+        ip_addr = "156.74.181.208"
+        details = get_IP_details(ip_addr, access_token)
+        self.assertEqual(details.city, "Seattle")
+
+    def test_load_data(self):
+        load_data()
+        self.assertEqual(Visitor.objects.count(), 50)
+        self.assertIsInstance(Visitor.objects.all()[0], Visitor)
