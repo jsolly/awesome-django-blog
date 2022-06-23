44bc1583bfabef5e75a325d1cafab75004090ae3
John Solly <jsolly@protonmail.com>
2022-06-23 08:19:01 -0700

    register visitor model on admin page

--- django_project/siteanalytics/admin.py
+++ django_project/siteanalytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Visitor
 
-# Register your models here.
+
+@admin.register(Visitor)
+class VisitorAdmin(OSMGeoAdmin):
+    list_display = ("ip_addr", "location")
