7b92346c35bf4aaffb7c3974b3f04d5ae8eec6af
John Solly <jsolly@protonmail.com>
2022-06-28 10:29:00 -0700

    experiment

--- django_project/siteanalytics/middleware.py
+++ django_project/siteanalytics/middleware.py
@@ -7,7 +7,7 @@ def requestTrackMiddleware(get_response):
     def middleware(request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        add_ip_person_if_not_exist(request)
+        # add_ip_person_if_not_exist(request)
         response = get_response(request)
 
         # Code to be executed for each request/response after
