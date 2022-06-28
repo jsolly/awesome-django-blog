ccbc916cc6d8e3cbb276343f350d4ed203061ab9
John Solly <jsolly@protonmail.com>
2022-06-28 13:16:52 -0700

    test

--- django_project/siteanalytics/views.py
+++ django_project/siteanalytics/views.py
@@ -4,6 +4,7 @@ from .models import Visitor
 
 
 def site_analytics_view(request):
+    raise Exception
     return render(
         request, "blog/site_analytics.html", {"visitors": Visitor.objects.all()}
     )
