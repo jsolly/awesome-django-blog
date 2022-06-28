cf0ef4da0017fc6e618bb6eac3f35a6406567b2a
John Solly <jsolly@protonmail.com>
2022-06-28 10:35:01 -0700

    experiment

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -51,7 +51,7 @@ CSP_SCRIPT_SRC = (
     "'self'",
     "https://cdn.jsdelivr.net",
     "https://unpkg.com/",
-    "'sha256-31FSFpo5gKxHSroR0WNnnBl3DS7XnHiUJRb0L8DKYd8='"
+    "'sha256-ZekfmNMIUJgBZMbrVyV/R0groux8PEEiO3YRZhDFf9Q='"
 )
 CSP_IMG_SRC = ("'self'", "data:", "https://unpkg.com/", "*.openstreetmap.org")
 CSP_FONT_SRC = ("'self'",)
