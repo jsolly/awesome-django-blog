cf57c46fe4f2fd5427a2ddf976517fff729435a5
John Solly <jsolly@protonmail.com>
2022-06-29 12:37:58 -0700

    linting

--- django_project/siteanalytics/middleware.py
+++ django_project/siteanalytics/middleware.py
@@ -8,7 +8,7 @@ def requestTrackMiddleware(get_response):
     def middleware(request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        if os.environ["DEBUG"] == "False": #TODO remove when test fixtures comes
+        if os.environ["DEBUG"] == "False":  # TODO remove when test fixtures comes
             add_ip_person_if_not_exist(request)
         response = get_response(request)
 
