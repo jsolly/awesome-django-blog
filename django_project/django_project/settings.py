4c55a0d127ce51e6c275dc7d35001997b8f5b4c0
John Solly <jsolly@protonmail.com>
2022-06-28 19:57:24 -0700

    experiment

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -63,6 +63,7 @@ CSP_FORM_ACTION = ("'self'", "https://blogthedata.us14.list-manage.com")
 CSP_OBJECT_SRC = ("'none'",)
 CSP_EXCLUDE_URL_PREFIXES = ("/admin", "/site-analytics")
 # CSP_REQUIRE_TRUSTED_TYPES_FOR = ("'script'",)
+USE_SRI = False
 if os.environ["DEBUG"] == "True":
     # USE_SRI = True
     CSP_EXCLUDE_URL_PREFIXES = (
