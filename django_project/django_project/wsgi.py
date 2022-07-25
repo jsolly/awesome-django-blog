2145682a2f99bed31a4be14451ab3aaae4aa4c6a
John Solly <jsolly@protonmail.com>
2022-07-25 06:25:35 -0700

    change wsgi file to point to prod settings

--- django_project/django_project/wsgi.py
+++ django_project/django_project/wsgi.py
@@ -14,6 +14,6 @@ load_dotenv("/home/jsolly/blogthedata/.env")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings.prod")
 
 application = get_wsgi_application()
