f918cd84264cde8d925b2f5e24c1d102da1fe5f7
John Solly <jsolly@protonmail.com>
2022-07-26 10:07:35 -0700

    wip

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -55,6 +55,6 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             slugify_instance_title(self, save=False)
-        self.metaimg_mimetype = filetype.guess(self.metaimg.path).MIME
+        self.metaimg_mimetype = filetype.guess(self.metaimg).MIME
 
         super().save(*args, **kwargs)
