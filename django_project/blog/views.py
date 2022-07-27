c920d137affbc57a0ab1e172f0a8ce8a9e872b03
John Solly <jsolly@protonmail.com>
2022-07-27 07:03:47 -0700

    increase pagination count to 10

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -19,7 +19,7 @@ class HomeView(ListView):
     model = Post
     template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
     context_object_name = "posts"  # The default is object_list
-    paginate_by = 5
+    paginate_by = 10
 
     def get_queryset(self):
         if self.request.user.is_staff or self.request.user.is_superuser:
@@ -31,7 +31,7 @@ class UserPostListView(ListView):  # Not actively worked on
     model = Post
     template_name = "blog/post/user_posts.html"  # <app>/<model>_<viewtype>.html
     context_object_name = "posts"  # The default is object_list
-    paginate_by = 5
+    paginate_by = 10
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get("username"))
@@ -97,7 +97,7 @@ class CategoryView(ListView):
     model = Post
     template_name = "blog/post/categories.html"  # <app>/<model>_<viewtype>.html
     context_object_name = "posts"  # The default is object_list
-    paginate_by = 5
+    paginate_by = 10
 
     def get_queryset(self):
         category = self.kwargs.get("category").replace("-", " ")
@@ -117,7 +117,7 @@ class CategoryView(ListView):
 
 @csrf_exempt
 def search_view(request):
-    """Controls what is shown to a user when they search for a post. A note...I never bothered to make sure admins could see draft posts in this view"""
+    """Controls what is shown to a user when they search for a post.w"""
     if request.method == "POST":
         searched = request.POST["searched"]
         posts = Post.objects.active()
