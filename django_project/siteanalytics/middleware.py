e32d3697b3bcb4667fd2fbb4ad5ad580871e9d80
John Solly <jsolly@protonmail.com>
2022-08-14 00:39:10 -0700

    linting

--- django_project/siteanalytics/middleware.py
+++ django_project/siteanalytics/middleware.py
@@ -1,13 +1,12 @@
 from .utils import add_visitor_if_not_exist
 from django.conf import settings
 
-def requestTrackMiddleware(get_response):
-    # One-time configuration and initialization.
 
+def requestTrackMiddleware(get_response):
     def middleware(request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        if settings.SETTINGS_MODULE == 'django_project.settings.prod':
+        if settings.SETTINGS_MODULE == "django_project.settings.prod":
             add_visitor_if_not_exist(request)
         response = get_response(request)
 
