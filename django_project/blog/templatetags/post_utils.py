04e2469ef6fcd74ced516025f1c9f27b84faa1c1
John Solly <jsolly@umd.edu>
2023-01-22 20:18:15 -0800

    Fix html parsing issue with readtime library

--- django_project/blog/templatetags/post_utils.py
+++ django_project/blog/templatetags/post_utils.py
@@ -1,11 +1,13 @@
 from django import template
 import readtime
+import html
 
 register = template.Library()
 
 
-def read(html):
-    return readtime.of_html(html)
+def read(input_html):
+    clean_html = html.escape(input_html)
+    return readtime.of_html(clean_html)
 
 
 register.filter("readtime", read)
