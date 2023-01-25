da1765f3298500f6d45cddd242be9be7b7da1086
John Solly <jsolly@protonmail.com>
2023-01-24 23:01:59 -0800

    Add Header copy links via external JavaScript file (#185)
    Co-authored-by: John Solly <jsolly@umd.edu>

--- django_project/staticfiles/js/addHeaderIds.js
+++
@@ -1,8 +0,0 @@
-function addHeaderIDs() {
-  var headers = document.querySelectorAll("h2, h3");
-  headers.forEach(header => {
-    header.id = header.textContent.toLowerCase().replace(/\s+/g, '-').replace(/[^a-zA-Z0-9_\-]/g,'');
-  });
-}
-
-window.onload = addHeaderIDs;
