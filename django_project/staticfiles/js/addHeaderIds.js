d0f329950ebf7302719500e149965780cb32210e
John Solly <jsolly@umd.edu>
2023-01-24 21:57:26 -0800

    Add id of "top" to a parent container of the breadcrumbs
    On the home and category pages, the post-detail urls now append #top so that the user is scrolled passed the navigation when they open a new blog post.

--- django_project/staticfiles/js/addHeaderIds.js
+++ django_project/staticfiles/js/addHeaderIds.js
@@ -1,5 +1,5 @@
 function addHeaderIDs() {
-  var headers = document.querySelectorAll("h1, h2, h3");
+  var headers = document.querySelectorAll("h2, h3");
   headers.forEach(header => {
     header.id = header.textContent.toLowerCase().replace(/\s+/g, '-').replace(/[^a-zA-Z0-9_\-]/g,'');
   });
