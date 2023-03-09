adbc5f9dd892c95f2d84d6ffadc4e2b077639edf
John Solly <jsolly@umd.edu>
2023-03-08 23:58:41 -0800

    wip

--- django_project/staticfiles/js/utils.js
+++ django_project/staticfiles/js/utils.js
@@ -1,18 +1,5 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const chatBox = document.querySelector(".chatbox__support");
-//   let chatMessages = chatBox.querySelector(".chatbox__messages");
-//   let scrollPosition = 0;
-
-//   chatMessages.addEventListener("scroll", function () {
-//     scrollPosition = chatMessages.scrollTop;
-//   });
-
-//   document.body.addEventListener("htmx:afterSwap", function (evt) {
-//     if (evt.detail.trigger === "chatbox-submit") {
-//       const newChatMessages = chatBox.querySelector(".chatbox__messages");
-//       const newScrollHeight = newChatMessages.scrollHeight;
-//       chatBox.scrollTop = newScrollHeight - scrollPosition;
-//       chatMessages = newChatMessages;
-//     }
-//   });
+// document.body.addEventListener("htmx:afterSwap", function (evt) {
+//   if (evt.detail.trigger === "chatbox-submit") {
+//     console.log("chatbox-submit");
+//   }
 // });
