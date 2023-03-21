aedac97c8f8ee157e1ea3014b26a11dc7bb3020e
John Solly <jsolly@umd.edu>
2023-03-21 16:40:51 -0700

    Fix accordion open

--- django_project/staticfiles/js/accordion.js
+++ django_project/staticfiles/js/accordion.js
@@ -1,23 +1,20 @@
-// accordion.js
-
 function toggleAccordion(accordionHeader) {
-    const accordionItem = accordionHeader.parentElement;
-    const accordionCollapse = accordionItem.querySelector('.accordion-collapse');
-  
-    // Toggle the 'expanded' class
-    accordionCollapse.classList.toggle('expanded');
-  }
-  
-  function initAccordion() {
-    const accordionHeaders = document.querySelectorAll('.accordion-header');
-  
-    accordionHeaders.forEach(header => {
-      header.addEventListener('click', () => {
-        toggleAccordion(header);
-      });
+  const accordionItem = accordionHeader.parentElement;
+  const accordionCollapse = accordionItem.querySelector('.accordion-collapse');
+
+  // Toggle the 'expanded' class
+  accordionCollapse.classList.toggle('expanded');
+}
+
+function initAccordion() {
+  const accordionButtons = document.querySelectorAll('.accordion-button');
+
+  accordionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      toggleAccordion(button.parentElement);
     });
-  }
-  
-  // Initialize the accordion when the window has finished loading
-  window.addEventListener('load', initAccordion);
-  
+  });
+}
+
+// Initialize the accordion when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', initAccordion);
