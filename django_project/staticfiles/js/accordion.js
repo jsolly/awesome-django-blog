3425bd924d933799daac341d3ec56b8a01492537
John Solly <jsolly@umd.edu>
2023-03-23 21:37:15 -0700

    Move accordion JS into seperate file

--- django_project/staticfiles/js/accordion.js
+++ django_project/staticfiles/js/accordion.js
@@ -1,22 +1,36 @@
-function toggleAccordion(accordionHeader) {
-  const accordionItem = accordionHeader.parentElement;
-  const accordionCollapse = accordionItem.querySelector('.accordion-collapse');
-  const accordionIcon = accordionHeader.querySelector('.accordion-icon');
-
-  // Toggle the 'expanded' class
-  accordionCollapse.classList.toggle('expanded');
-  accordionIcon.classList.toggle('expanded');
-}
+document.addEventListener('DOMContentLoaded', function () {
+  initAccordion();
+});
 
 function initAccordion() {
-  const accordionButtons = document.querySelectorAll('.accordion-button');
-
-  accordionButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      toggleAccordion(button.parentElement);
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', function () {
+      toggleAccordion(header);
     });
   });
 }
 
-// Initialize the accordion when the DOM content is loaded
-document.addEventListener('DOMContentLoaded', initAccordion);
+function toggleAccordion(accordionHeader) {
+  const accordionItem = accordionHeader.parentElement;
+  const accordionCollapse = accordionItem.querySelector('.accordion-collapse');
+  const accordionButton = accordionHeader.querySelector('.accordion-button');
+  console.log(accordionButton);
+
+  // Calculate the height of the accordion body
+  const accordionBody = accordionCollapse.firstElementChild;
+  const expandedHeight = accordionBody.scrollHeight;
+
+  // Toggle the 'expanded' class
+  accordionCollapse.classList.toggle('expanded');
+
+  // Update aria-expanded attribute
+  accordionButton.setAttribute('aria-expanded', accordionCollapse.classList.contains('expanded'));
+
+  // Set the height for the accordion collapse
+  if (accordionCollapse.classList.contains('expanded')) {
+    accordionCollapse.style.height = expandedHeight + 'px';
+  } else {
+    accordionCollapse.style.height = '0';
+  }
+}
