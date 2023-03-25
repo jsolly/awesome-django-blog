ff3e2cc451ad4326710b2b9dfb265ce772ac1518
John Solly <jsolly@umd.edu>
2023-03-25 12:19:25 -0700

    Open categories on click in header

--- django_project/staticfiles/js/header.js
+++ django_project/staticfiles/js/header.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var hamburger = document.querySelector('.hamburger');
-  
-    hamburger.addEventListener('click', function () {
-      console.log('click');
-      toggleMenu();
+  var hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', function () {
+    console.log('click');
+    toggleMenu();
+  });
+
+  var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+
+  dropdownToggles.forEach(function (toggle) {
+    toggle.addEventListener('click', function (event) {
+      event.preventDefault();
+      var dropdownMenu = toggle.nextElementSibling;
+      dropdownMenu.classList.toggle('show');
     });
   });
-  
-  function toggleMenu() {
-    var menu = document.querySelector('.navbar-menu');
-    menu.classList.toggle('show-menu');
-    console.log(menu.classList); // Add this line
-  }
-  
+});
+
+function toggleMenu() {
+  var menu = document.querySelector('.navbar-menu');
+  menu.classList.toggle('show-menu');
+  console.log(menu.classList);
+}
