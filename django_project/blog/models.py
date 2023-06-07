b397ffae0023d3355849547804115c107229b3d0
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-07 21:19:49 +0100

    rename body field in Comments Model to body

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -100,7 +100,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    content = CKEditor5Field(blank=True, null=True, config_name="extends")
     date_posted = models.DateTimeField(auto_now_add=True)
 
     class Meta:
