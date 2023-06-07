af4429306f936cda3e855f0705549dd1dae664ee
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-07 22:23:32 +0100

    update Comment model

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -107,4 +107,4 @@ class Comment(models.Model):
         ordering = ["date_posted"]
 
     def __str__(self):
-        return "Comment {} by {}".format(self.author, self.title)
+        return "Comment \"{}\" {} by {}".format(self.content[3:20] , "... ",self.author)
