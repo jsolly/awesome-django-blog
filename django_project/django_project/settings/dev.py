7c7d66988833196398b923b645eff836c9a89a5b
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-14 11:47:55 +0100

    add base_settings and dev files to git

---
+++ django_project/django_project/settings/dev.py
@@ -0,0 +1,27 @@
+from .base_settings import *
+import os
+
+SITE_ID = 2  # localhost
+CAPTCHA_TEST_MODE = True
+USE_SRI = False
+
+# HTTPS SETTINGS
+SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_HTTPONLY = False
+CSRF_COOKIE_HTTPONLY = False
+CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
+SECURE_BROWSER_XSS_FILTER = False
+SECURE_CONTENT_TYPE_NOSNIFF = False
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blogthedata",
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASS"],
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
