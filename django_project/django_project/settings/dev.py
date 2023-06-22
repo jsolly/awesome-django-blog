8ece8742f1c8cf07fd8314549b100639d081ba0d
John Solly <jsolly@umd.edu>
2023-06-21 20:22:46 -0700

    Add titles and descriptions to pages

--- django_project/django_project/settings/dev.py
+++ django_project/django_project/settings/dev.py
@@ -4,6 +4,7 @@ import os
 SITE_ID = 2  # localhost
 CAPTCHA_TEST_MODE = True
 USE_SRI = False
+# FASTDEV_STRICT_IF = True
 
 # HTTPS SETTINGS
 SESSION_COOKIE_SECURE = False
@@ -25,3 +26,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+INSTALLED_APPS += [
+    "django_fastdev",
+]
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -49,22 +49,58 @@ def profile_view(request):
 class MyLoginView(auth_views.LoginView):
     template_name = "users/login.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = "Login | Blogthedata.com"
+        context["description"] = "Login to your account"
+        return context
+
 
 class MyLogoutView(auth_views.LogoutView):
     template_name = "users/logout.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = "Logout | Blogthedata.com"
+        context["description"] = "Logout of your account"
+        return context
 
 
 class MyPasswordResetView(auth_views.PasswordResetView):
     template_name = "users/password_reset.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = "Reset Password | Blogthedata.com"
+        context["description"] = "Reset your password"
+        return context
+
 
 class MyPasswordResetDoneView(auth_views.PasswordResetDoneView):
     template_name = "users/password_reset_done.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = "Reset Password Email Sent | Blogthedata.com"
+        context["description"] = "Your password reset email has been sent!"
+        return context
 
 
 class MyPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
     template_name = "users/password_reset_confirm.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = "Reset Password Confirm | Blogthedata.com"
+        context["description"] = "Are you sure you want to reset your password?"
+        return context
+
 
 class MyPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
     template_name = "users/password_reset_complete.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = "Reset Password Complete | Blogthedata.com"
+        context["description"] = "Your password has been reset!"
+        return context
