02473cccc9447d9bbba680e2fe3e1769c10ea14a
John Solly <jsolly@umd.edu>
2023-06-23 12:11:01 -0700

    Resolve error if a post has no content

--- django_project/blog/templatetags/post_utils.py
+++ django_project/blog/templatetags/post_utils.py
@@ -6,6 +6,10 @@ register = template.Library()
 
 
 def read(input_html):
+    # Check if input_html is None or empty
+    if not input_html:
+        return "0 minutes"
+
     clean_html = html.escape(input_html)
     return readtime.of_html(clean_html)
 
