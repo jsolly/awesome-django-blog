b339cf650c3901ac2018abdd06eda57d6a863e81
Praise Freedom Dike <dikepraise119@gmail.com>
2023-07-24 00:05:03 +0100

    delete view import

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -11,7 +11,6 @@ from django.views.generic import (
     CreateView,
     UpdateView,
     DeleteView,
-    View,
 )
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
