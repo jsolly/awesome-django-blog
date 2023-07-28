1a75ebfc5f7dbd7d03f01395993086cdcddb0a9a
Praise Freedom Dike <dikepraise119@gmail.com>
2023-07-28 21:30:31 +0100

    delete second redirect in user utils function

--- django_project/users/utils.py
+++ django_project/users/utils.py
@@ -7,10 +7,7 @@ def handle_no_permission(request, slug):
     Redirects the user to the login page, and after successful login, redirects
     back to the original page with the appropriate slug to submit the comment.
     """
-    login_url = f"/login/?next=/post/{slug}/" + "#comments-section"
+    login_url = f"/login/?next=/post/{slug}/#comments-section"
 
     if not request.user.is_authenticated:
         return redirect(login_url)
-
-    # If the user is logged in, redirect to the original page to submit the comment
-    return redirect("post-detail", slug=slug)
