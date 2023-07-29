de1b400b3feb8a5074a27809623aa69aa771e1dc
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-07-29 05:00:29 +0100

    Feature/redirect after comment login (#330)
    * add conditional to add_comment form action
    
    * add conditional to add_comment form action
    
    * configure LoginRequiredMixin for comment
    
    * run black tests
    
    * delete second redirect in user utils function
    
    * remove conditional from create-comment form

---
+++ django_project/users/utils.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+
+
+def handle_no_permission(request, slug):
+    """
+    Handles the case when the user is not logged in and tries to access the view.
+    Redirects the user to the login page, and after successful login, redirects
+    back to the original page with the appropriate slug to submit the comment.
+    """
+    login_url = f"/login/?next=/post/{slug}/#comments-section"
+
+    if not request.user.is_authenticated:
+        return redirect(login_url)
