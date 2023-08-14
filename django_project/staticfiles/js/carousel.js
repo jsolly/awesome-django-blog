1e6f82c7066739ecf1b5a4d2be8abecc1000a334
John Solly <jsolly@umd.edu>
2023-08-13 21:56:38 -0700

    formatting

--- django_project/staticfiles/js/carousel.js
+++ django_project/staticfiles/js/carousel.js
@@ -1,49 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   let currentCarouselIndex = 0;
-  const carouselItems = document.querySelectorAll('.carousel-item');
-  const indicators = document.querySelectorAll('.carousel-indicators li');
+  const carouselItems = document.querySelectorAll(".carousel-item");
+  const indicators = document.querySelectorAll(".carousel-indicators li");
 
   function updateIndicators() {
     indicators.forEach(function (indicator, index) {
       if (index === currentCarouselIndex) {
-        indicator.classList.add('active');
+        indicator.classList.add("active");
       } else {
-        indicator.classList.remove('active');
+        indicator.classList.remove("active");
       }
     });
   }
 
   function changeCarouselItem(step) {
-    carouselItems[currentCarouselIndex].classList.remove('active');
-    currentCarouselIndex = (currentCarouselIndex + step + carouselItems.length) % carouselItems.length;
-    carouselItems[currentCarouselIndex].classList.add('active');
+    carouselItems[currentCarouselIndex].classList.remove("active");
+    currentCarouselIndex =
+      (currentCarouselIndex + step + carouselItems.length) %
+      carouselItems.length;
+    carouselItems[currentCarouselIndex].classList.add("active");
     updateIndicators();
   }
 
   function changeCarouselItemTo(index) {
     if (index === currentCarouselIndex) return;
-    carouselItems[currentCarouselIndex].classList.remove('active');
+    carouselItems[currentCarouselIndex].classList.remove("active");
     currentCarouselIndex = index;
-    carouselItems[currentCarouselIndex].classList.add('active');
+    carouselItems[currentCarouselIndex].classList.add("active");
     updateIndicators();
   }
 
-  const prevControl = document.querySelector('.carousel-control-prev');
-  const nextControl = document.querySelector('.carousel-control-next');
+  const prevControl = document.querySelector(".carousel-control-prev");
+  const nextControl = document.querySelector(".carousel-control-next");
 
-  prevControl.addEventListener('click', function () {
+  prevControl.addEventListener("click", function () {
     changeCarouselItem(-1);
   });
 
-  nextControl.addEventListener('click', function () {
+  nextControl.addEventListener("click", function () {
     changeCarouselItem(1);
   });
 
-  const indicatorItems = document.querySelectorAll('[data-carousel-index]');
+  const indicatorItems = document.querySelectorAll("[data-carousel-index]");
 
-  indicatorItems.forEach(item => {
-    item.addEventListener('click', function () {
-      const index = parseInt(this.getAttribute('data-carousel-index'));
+  indicatorItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      const index = parseInt(this.getAttribute("data-carousel-index"));
       changeCarouselItemTo(index);
     });
   });
