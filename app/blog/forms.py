9689486f42b50ab8f2027cacec7cb589b0031a5b
Praise Freedom Dike <dikepraise119@gmail.com>
2023-11-08 08:05:43 +0100

    attempt to customise draft checkbox styling

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -29,6 +29,7 @@ class PostForm(forms.ModelForm):
             "metadesc": forms.TextInput(),
             "metaimg_alt_txt": forms.TextInput(),
             "metaimg_attribution": forms.TextInput(),
+            "draft": forms.CheckboxInput(attrs={"class": "custom-checkbox-input"}),
         }
 
     def __init__(self, *args, **kwargs):
