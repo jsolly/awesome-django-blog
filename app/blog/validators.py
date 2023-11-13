746c5257321a6ff7d8102b670a0082304ab85245
John Solly <jsolly@protonmail.com>
2023-11-13 13:58:43 -0500

    dependency injection, explicit True return

--- app/blog/validators.py
+++ app/blog/validators.py
@@ -4,12 +4,12 @@ from django.core.exceptions import ValidationError
 link_media_pattern = re.compile(
     r"<a.*?/a>|<img.*?/img>|<video.*?/video>|<audio.*?/audio>", flags=re.IGNORECASE
 )
-max_length = 400
 
 
-def snippet_validator(value):
+def snippet_validator(value, max_length=400):
     value_without_links_media = link_media_pattern.sub("", value)
     if len(value_without_links_media) > max_length:
         raise ValidationError(
             f"The snippet cannot have more than {max_length} characters (excluding links and media)."
         )
+    return True
