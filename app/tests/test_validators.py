9dcf6c08d0193253c5b720efefa182ff23be59bd
John Solly <jsolly@protonmail.com>
2023-11-13 14:16:31 -0500

    Add failing test

--- app/tests/test_validators.py
+++ app/tests/test_validators.py
@@ -7,16 +7,23 @@ from django.core.exceptions import ValidationError
 
 
 class TestValidators(SetUp):
-    def test_snippet_validation(self):
+    def test_snippet_validation_valid_with_links(self):
         max_length = 400
         valid_value_with_links = " ".join(
             [f'<a href="http://example{i}.com">Link{i}</a>' for i in range(100)]
         )
-        invalid_value = f"""A {'<a href="http://example.com">Link</a> ' * 10}{'B' * (max_length - 10)}"""
-
-        with self.assertRaises(ValidationError):
-            snippet_validator(invalid_value, max_length=max_length)
 
         self.assertTrue(
             snippet_validator(valid_value_with_links, max_length=max_length)
         )
+
+    def test_snippet_validation_valid_formatted_text(self):
+        invalid_value = """<p><span style="background-color:rgb(255,255,255);color:rgba(0,0,0,0.9);"><span style="-webkit-text-stroke-width:0px;display:inline !important;float:none;font-family:-apple-system, system-ui, &quot;system-ui&quot;, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, &quot;Fira Sans&quot;, Ubuntu, Oxygen, &quot;Oxygen Sans&quot;, Cantarell, &quot;Droid Sans&quot;, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Lucida Grande&quot;, Helvetica, Arial, sans-serif;font-size:14px;font-style:normal;font-variant-caps:normal;font-variant-ligatures:normal;font-weight:400;letter-spacing:normal;orphans:2;text-align:start;text-decoration-color:initial;text-decoration-style:initial;text-decoration-thickness:initial;text-indent:0px;text-transform:none;white-space:normal;widows:2;word-spacing:0px;">Kick-start your career in geospatial sciences with our Remote Sensing Internship Program! Dive into hands-on projects, learn from industry experts, and make your mark on real-world applications. Apply now and shape the future of remote sensing!</span></span></p>"""
+
+        self.assertTrue(snippet_validator(invalid_value, max_length=400))
+
+    def test_snippet_validation_invalid(self):
+        max_length = 400
+        invalid_value = f"""A {'<a href="http://example.com">Link</a> ' * 10}{'B' * (max_length - 10)}"""
+        with self.assertRaises(ValidationError):
+            snippet_validator(invalid_value, max_length=max_length)
