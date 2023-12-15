1fcf39f3bf3fe9516f62ca33b3960ca6bf771dea
Praise Freedom Dike <dikepraise119@gmail.com>
2023-12-15 09:08:26 +0100

    add upvotes field to Post model

--- app/blog/models.py
+++ app/blog/models.py
@@ -71,6 +71,7 @@ class Post(models.Model):
     metaimg_attribution = models.CharField(max_length=500, blank=True, null=True)
     content = CKEditor5Field(blank=True, null=True)
     snippet = CKEditor5Field(blank=True, null=True)
+    upvotes = models.IntegerField(default=0)
     date_posted = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
