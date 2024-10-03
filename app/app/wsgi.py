8e47ce8e3df15fb24dc3c5d3fe1e337f215c1c75
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-03 11:43:31 -0400

    wip

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,4 +1,7 @@
 from django.core.wsgi import get_wsgi_application
+import os
+
+print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "app.settings.notreal"
 application = get_wsgi_application()
