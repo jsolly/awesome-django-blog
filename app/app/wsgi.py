7c15ed106fc7891cd5e9c80636c44aa18423b673
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-03 09:13:03 -0400

    wip

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.app.settings.prod")
 
 application = get_wsgi_application()
