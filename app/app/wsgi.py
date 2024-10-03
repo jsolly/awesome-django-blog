8e266eb6228fdbd0a96d32d08fcd74643da0595b
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-03 09:19:15 -0400

    wip

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,7 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.app.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.prod")
 
 application = get_wsgi_application()
--- app/manage.py
+++ app/manage.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import sys
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
 
-load_dotenv()
+if "DYNO" not in os.environ:
+    load_dotenv()
+
 
 def setup_env():
-    env_example_path = Path('.') / '.env.example'
-    env_path = Path('.') / '.env'
+    env_example_path = Path(".") / ".env.example"
+    env_path = Path(".") / ".env"
 
     if env_path.exists():
         print(".env file already exists. Exiting.")
@@ -20,24 +23,27 @@ def setup_env():
 
     secret_key = get_random_secret_key()
 
-    with open(env_example_path, 'r') as example_file:
+    with open(env_example_path, "r") as example_file:
         env_content = example_file.readlines()
 
-    with open(env_path, 'w') as env_file:
+    with open(env_path, "w") as env_file:
         for line in env_content:
-            key_value = line.strip().split('=', 1)
-            if key_value[0] == 'SECRET_KEY':
-                env_file.write(f'SECRET_KEY={secret_key}\n')
+            key_value = line.strip().split("=", 1)
+            if key_value[0] == "SECRET_KEY":
+                env_file.write(f"SECRET_KEY={secret_key}\n")
             else:
-                env_file.write(key_value[0] + '=' + key_value[1].split('#')[0].strip() + '\n')
+                env_file.write(
+                    key_value[0] + "=" + key_value[1].split("#")[0].strip() + "\n"
+                )
 
     print(".env file created with a new SECRET_KEY and other values from .env.example.")
 
+
 # Make sure DJANGO_SETTINGS_MODULE is added to .env file
 def main():
-    if 'setup_env' in sys.argv:
+    if "setup_env" in sys.argv:
         setup_env()
-        sys.argv.remove('setup_env')
+        sys.argv.remove("setup_env")
 
     try:
         from django.core.management import execute_from_command_line
