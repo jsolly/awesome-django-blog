f5cddf26fd5255ccd68c4de60c837060944decd1
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-03 11:50:27 -0400

    wip

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,7 +1,11 @@
-from django.core.wsgi import get_wsgi_application
 import os
+from django.core.wsgi import get_wsgi_application
 
+# Set the DJANGO_SETTINGS_MODULE environment variable before calling get_wsgi_application
+os.environ["DJANGO_SETTINGS_MODULE"] = "app.settings.notreal"
+
+# Print it for debugging
 print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "app.settings.notreal"
+# Load the application
 application = get_wsgi_application()
