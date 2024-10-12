521940c0cf959c9ffb56006c83062e4a9081964e
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-12 11:58:13 -0400

    wip

--- app/asgi.py
+++ app/asgi.py
@@ -1,3 +1,6 @@
 from django.core.asgi import get_asgi_application
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_asgi_application()
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,3 +1,6 @@
 from django.core.wsgi import get_wsgi_application
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
