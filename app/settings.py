cd60f589e38e36228d3e1c1158d5e4be1e50550c
John Solly <9572232+jsolly@users.noreply.github.com>
2024-11-02 21:07:23 -0400

    testing

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,7 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     # SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
     # HSTS SETTINGS (Configured in CloudFlare)
